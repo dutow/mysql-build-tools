19810d44e6e552a9d7d6a22d71d713079ae3956f
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2019-09-02 22:56:57 +0200

    Installer improvements
    * Check for the existence of the pipenv command
    * Only specify python 3 (default is 3.7 with ubuntu disco)
    * Add path change to .profile instead of .bashrc
    * Added automatic mbt docker network creation if docker is available

--- install.sh
+++ install.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+if ! [ -x pipenv ]; then
+  echo "pipenv is not available, install it first."
+  echo "on ubuntu: apt install pipenv"
+  exit 1
+fi
+
+if ! [ -x docker ]; then
+  echo "docker is not available."
+  echo "You should create the mbt network later using:"
+  echo "docker network create mbt"
+fi
+
 cd ~
 git clone https://github.com/dutow/mysql-build-tools.git
-echo "export PATH=\$PATH:`pwd`/mysql-build-tools/bin" >> ~/.bashrc
+echo "export PATH=\$PATH:`pwd`/mysql-build-tools/bin" >> ~/.profile
 cd mysql-build-tools
-PIPENV_PIPFILE="`pwd`/bin/../Pipfile" pipenv install --python 3.6
+PIPENV_PIPFILE="`pwd`/bin/../Pipfile" pipenv install --python 3
+if [ -x docker ]; then
+  docker network create mbt
+fi
 echo "==========================="
 echo "MBT installed. Now:"
 echo " * Initialize mbt:"
